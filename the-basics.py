c74480535589b4dfabe6c89216387b3c4cd11c84
Athitheone <thanos.alexandrou12@gmail.com>
2025-03-08 23:01:47 +0000

    sad

---
+++ the-basics.py
@@ -0,0 +1,49 @@
+#name = input("Wie heißt du? ")
+#print("Hallo, " + name + "! Willkommen in der Python Welt") #Nach name fragen und danach begrüssen
+
+#try:
+    #alter = float(input("Wie alt bist du? ")) # Umwandlung von string zu interger
+    #print("In 10 Jahen wirst du", alter + 10, "Jahre alt sein!")
+
+#except ValueError:
+    #print("Bitte gib eine gültige Zahl ein")
+
+
+#alter = float(input("Wie alt bist du?"))
+#if alter >= 18:
+    #print("Du bist volljährig")
+#else:
+    #print("Du bist noch nicht volljährig") 
+
+#while True:
+    #eingabe = input("Du: ")
+    
+    #if eingabe.lower() == "bye":  # .lower verwandelt alle Buchstaben eines Textes in kleine Buchsatben um
+        #print("J.A.R.V.I.S: Auf Wiedersehen!") # () wichtig um die Methode auszuführen
+    #break  # Beendet die Schleife # == überprufen ob die Werte gleich sind
+
+#print("J.A.R.V.I.S: Du hast gesagt:", eingabe)
+
+#while True:
+    #eingabe = input("Du: ").lower()
+
+    #if eingabe == "hallo j.a.r.v.i.s":
+        #print("J.A.R.V.I.S: Hallo! Wie kann ich helfen?")
+
+    #elif eingabe == "wie geht es dir?":
+        #print("J.A.R.V.I.S: Mir geht es gut danke der Nachfrage!")    
+
+    #elif eingabe == "was kannst du?":
+        #print("J.A.R.V.I.S: Ich kann mit dir sprechen und dir helfen Python zu lernen")    
+
+    #elif eingabe == "bye":
+        #print("J.A.R.V.I.S: Auf Wiedersehen!")
+        #break
+
+    #else:
+        #print("j.a.r.v.i.s: Das verstehe ich leider nicht.")
+
+
+
+
+        
