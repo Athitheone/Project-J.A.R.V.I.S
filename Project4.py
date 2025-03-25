c9229c79197aa7b47ee486bae7c2531f66e237e1
Athitheone <thanos.alexandrou12@gmail.com>
2025-03-25 11:30:23 +0000

    sads

---
+++ Project4.py
@@ -0,0 +1,9 @@
+def rechnen(a , b):
+    return z1 + z2 
+
+Rechnung = input("+ ,*, /, -")
+z1 = float(input("Zahl 1: "))
+z2 = float(input("Zahl 2: "))
+ergebniss = print(rechnen(z1 ,z2 ,"+"))
+
+if Rechnung == "*":
